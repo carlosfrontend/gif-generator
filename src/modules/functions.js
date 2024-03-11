340d63ed021d0577a0446ae224d2e5965c224e79
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-03-11 21:02:18 +0100

    Feat: Refatorized for the the use of async/await

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,33 +1,32 @@
-
 export const handleApiKeyError = (response) => {
-    if (!response.ok) {
-        throw new Error(
-          'Could not obtain a new image. A correct API key is required!!'
-        );
-      }
+  if (!response.ok) {
+    throw new Error(
+      'Could not obtain a new image. A correct API key is required!!'
+    );
+  }
 };
 
-export const showError = (span,error) => {
-    const spanError = span;
-    spanError.textContent = error;
+export const showError = (span, error) => {
+  const spanError = span;
+  spanError.textContent = error;
 };
 
 export const setGif = (img, response) => {
-    const imgTag = img;
-    imgTag.src = response.data.images.original.url;
+  const imgTag = img;
+  imgTag.src = response.data.images.original.url;
 };
 
 export const handleGifNotFound = (response) => {
-    if (response.data.length === 0 && response.meta.status === 200) {
-        throw new Error(
-          'Try writing something different. We did not find images with that term!!'
-        );
-      }
+  if (response.data.length === 0 && response.meta.status === 200) {
+    throw new Error(
+      'Try writing something different. We did not find images with that term!!'
+    );
+  }
 };
 
 export const clearError = (span) => {
-    const spanError = span;
-    spanError.textContent = '';
+  const spanError = span;
+  spanError.textContent = '';
 };
 
 export const clearInputValue = (event) => {
--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -14,43 +14,44 @@ const spanError = document.querySelector('.error');
 const img = document.querySelector('.result');
 img.src = defaultImg;
 
-export const getRandomGif = () => {
-  fetch(URI, { mode: 'cors' })
-    .then((response) => {
-      handleApiKeyError(response);
-      clearError(spanError);
-      return response.json();
-    })
-    .then((response) => {
-      setGif(img, response);
-    })
-    .catch((error) => {
-      showError(spanError, error);
+export const getRandomGif = async () => {
+  try {
+    const response = await fetch(URI, { mode: 'cors' });
+    handleApiKeyError(response);
+    clearError(spanError);
+    response.json().then((res) => {
+      setGif(img, res);
     });
+  } catch (error) {
+    showError(spanError, error);
+  }
 };
 
-export const searchGif = (event) => {
+export const searchGif = async (event) => {
   const myEvent = event;
-  const searchInput = document.querySelector('#search');
-  const searchInputValue = searchInput.value;
-  const URI_SEARCH = `${URI.replace(
-    'random',
-    'translate'
-  )}&s=${searchInputValue}`;
-  fetch(URI_SEARCH, { mode: 'cors' })
-    .then((response) => {
-      handleApiKeyError(response);
-      clearError(spanError);
-      return response.json();
-    })
-    .then((response) => {
-      handleGifNotFound(response);
-      setGif(img, response);
-      clearError(spanError);
-      clearInputValue(myEvent);
-    })
-    .catch((error) => {
-      showError(spanError, error);
-      clearInputValue(myEvent);
+  try {
+    const searchInput = document.querySelector('#search');
+    const searchInputValue = searchInput.value;
+    const URI_SEARCH = `${URI.replace(
+      'random',
+      'translate'
+    )}&s=${searchInputValue}`;
+
+    const response = await fetch(URI_SEARCH, { mode: 'cors' });
+    handleApiKeyError(response);
+    clearError(spanError);
+    response.json().then((res) => {
+      try {
+        handleGifNotFound(res);
+        setGif(img, res);
+        clearError(spanError);
+        clearInputValue(myEvent);
+      } catch (error) {
+        showError(spanError, error);
+        clearInputValue(myEvent);
+      }
     });
+  } catch (error) {
+    showError(spanError, error);
+  }
 };
